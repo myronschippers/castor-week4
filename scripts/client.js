e50c73aa726c2e82487c1b4a6c518b802ad0b65b
Myron <dubtwee@gmail.com>
2020-09-17 18:47:21 -0500

    adding code samples from

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,34 @@
 console.log('Hello Castor');
+
+// anonymous function
+// function () {}
+
+// declared function
+function myFunction(name, lastName) {
+  console.log('in myFunction');
+  return name + ' ' + lastName;
+}
+
+// const myOtherFunction = function () {};
+
+// function call
+const result = myFunction('Myron', 'Schippers'); // 1 2
+console.log(result);
+
+const basketOfFruit = ['orange', 'apple', 'grapefruit'];
+
+const loopFruit = function (listOfFruit, nameFruit) {
+  for (let fruit of listOfFruit) {
+    // console.log(fruit)
+    // want to see specific fruit
+    if (fruit === nameFruit) {
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+console.log(loopFruit(basketOfFruit, 'apple'));
+console.log(loopFruit(['kiwi', 'passion fruit'], 'apple'));
